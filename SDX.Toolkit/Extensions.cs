f9b5540dead32965d09a507aec4fd7e056a57656
Patrick Strader <patrick_strader@hotmail.com>
2018-08-21 15:18:13 -0700

    Reworking this project. Rather than trying to convert all the old controls, we'll just build the new ones here using the old ones as examples and pulling some source from them.

---
+++ SDX.Toolkit/Extensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+namespace SDX.Toolkit
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Removes all instances of a type of object from the children collection.
+        /// </summary>
+        /// <typeparam name="T">The type of object you want to remove.</typeparam>
+        /// <param name="targetCollection">A reference to the canvas you want items removed from.</param>
+        public static void Remove<T>(this UIElementCollection targetCollection)
+        {
+            // This will loop to the end of the children collection.
+            int index = 0;
+
+            // Loop over every element in the children collection.
+            while (index < targetCollection.Count)
+            {
+                // Remove the item if it's of type T
+                if (targetCollection[index] is T)
+                    targetCollection.RemoveAt(index);
+                else
+                    index++;
+            }
+        }
+    }
+}
