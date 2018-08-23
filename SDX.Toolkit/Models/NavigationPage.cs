81461fb90f2a4d07db2a1df19b84769666931e71
Patrick Strader <patrick_strader@hotmail.com>
2018-08-23 13:54:30 -0700

    NavigationBar first pass.

---
+++ SDX.Toolkit/Models/NavigationPage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDX.Toolkit.Models
+{
+    public class NavigationPage
+    {
+        public string Name;
+        public string Text;
+        public string ParentId;
+        public int Order;
+    }
+}
