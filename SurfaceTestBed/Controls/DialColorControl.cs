406e0df42243a3636421c52ae250a6346d306691
Jonathan Tiritilli <jon.efmagency@outlook.com>
2018-08-22 17:28:12 -0700

    creating colordial controller

---
+++ SurfaceTestBed/Controls/DialColorControl.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace SurfaceTestBed.Controls
+{
+    public sealed partial class DialColorControl : UserControl
+    {
+        #region Private Members
+        private bool rightHanded;
+        private byte[] bmpBytes = null;
+        private BitmapDecoder dec;
+        private Border _layoutRoot = null;
+        private Grid _layoutGrid = null;
+        private UserControl _userControl = null;
+
+        #endregion
+
+        #region Private Methods
+
+        private async void UpdateColorBrush()
+        {
+            int x = 0, y = 0;
+
+            double angle = (360 - Rotation) * Math.PI / 180;
+            double radius = 295;
+            //get the co ords of the pixel based on rotation
+            //x = radius cos t;
+            //y = radius sin t;
+            x = 350 + (int)(radius * Math.Cos(angle));
+            y = 353 + (int)(radius * Math.Sin(angle));
+
+            Color color = await GetSelectedColor(x, y);
+
+            Debug.WriteLine($"X: {x}, Y: {y} rotation: {Rotation} Color: {color.R}:{color.G}:{color.B}");
+
+            ColorBrush.Color = color;
+        }
+
+        private async Task<Color> GetSelectedColor(int x, int y)
+        {
+            Color color = new Color();
+            if (null == bmpBytes)
+            {
+                string imagelocation = @"Assets\custom_visual_colour_wheel.png";
+                StorageFolder InstallationFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+                StorageFile file = await InstallationFolder.GetFileAsync(imagelocation);
+                Stream imagestream = await file.OpenStreamForReadAsync();
+
+                dec = await BitmapDecoder.CreateAsync(imagestream.AsRandomAccessStream());
+
+                var data = await dec.GetPixelDataAsync();
+
+                bmpBytes = data.DetachPixelData();
+            }
+            color = GetPixel(bmpBytes, x, y, dec.PixelWidth, dec.PixelHeight);
+
+            return color;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public double Rotation
+        {
+            get { return (double)GetValue(RotationProperty); }
+            set { SetValue(RotationProperty, value); UpdateColorBrush(); }
+        }
+
+        // Using a DependencyProperty as the backing store for Rotation.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RotationProperty =
+            DependencyProperty.Register("Rotation", typeof(double), typeof(DialColorControl), new PropertyMetadata(0.0));
+
+        public SolidColorBrush ColorBrush
+        {
+            get { return (SolidColorBrush)GetValue(ColorBrushProperty); }
+            set { SetValue(ColorBrushProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ColorBrush.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ColorBrushProperty =
+            DependencyProperty.Register("ColorBrush", typeof(SolidColorBrush), typeof(DialColorControl), new PropertyMetadata(new SolidColorBrush(Colors.White)));
+
+        public Color GetPixel(byte[] pixels, int x, int y, uint width, uint height)
+        {
+            int i = y;
+            int j = x;
+            int k = (i * (int)width + j) * 4;
+            var b = pixels[k + 0];
+            var g = pixels[k + 1];
+            var r = pixels[k + 2];
+            return Color.FromArgb(255, r, g, b);
+        }
+
+        //public DialColorControl()
+        //{
+        //    //this.InitializeComponent();
+        //    //handedness for on-screen UI
+        //    Windows.UI.ViewManagement.UISettings settings = new Windows.UI.ViewManagement.UISettings();
+        //    rightHanded = (settings.HandPreference == Windows.UI.ViewManagement.HandPreference.RightHanded);
+        //    //if left handed then rotate the entire control 180
+        //    if (!rightHanded)
+        //    {
+        //        RotateTransform trans = new RotateTransform();
+        //        trans.Angle = 180;
+        //        colorControl.RenderTransform = trans;
+        //    }
+        //}
+
+        #endregion
+
+        #region UI Methods
+
+        public RenderUI()
+        {
+            // get the layoutroot
+            _layoutRoot = (Canvas)this.GetTemplateChild("dialCanvas");
+            if (null == _layoutRoot) { return; }
+
+            UserControl dialDisplay = new UserControl();
+            _layoutRoot.Children.Add(UserControl dialDisplay)
+        }
+        <UserControl x:Name="colorControl"
+            x:Class="DialDemo.Controls.DialColorControl"
+            xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+            xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+            xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+            xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+            mc:Ignorable="d"
+            d:DesignHeight="300"
+            d:DesignWidth="300"
+            RenderTransformOrigin="0.5,0.5">
+
+            <Grid>
+                <Image x:Name="ColorRingImage"  Source="ms-appx:///Assets/custom_visual_colour_wheel.png" RenderTransformOrigin="0.5,0.5" >
+                    <Image.RenderTransform>
+                        <CompositeTransform Rotation = "{Binding Rotation, ElementName=colorControl}" />
+                    </ Image.RenderTransform >
+                </ Image >
+            </ Grid >
+        </ UserControl >
+        #endregion
+    }
+}
--- SurfaceTestBed/Views/Page1Page.xaml.cs
+++ SurfaceTestBed/Views/Page1Page.xaml.cs
@@ -1,16 +1,36 @@
 ﻿using System;
-
-using Windows.UI;
-using Windows.UI.Xaml.Controls;
-
 using SurfaceTestBed.ViewModels;
 using SDX.Toolkit.Helpers;
-
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
+using Windows.UI;
+using Windows.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using SurfaceTestBed.Controls;
 
 namespace SurfaceTestBed.Views
 {
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
     public sealed partial class Page1Page : Page
     {
+        private RadialController myController;
+        private RadialControllerMenuItem screenColorMenuItem;
+        private DialColorControl dialControl;
+
         private Page1ViewModel ViewModel
         {
             get { return DataContext as Page1ViewModel; }
@@ -18,10 +38,112 @@ namespace SurfaceTestBed.Views
 
         public Page1Page()
         {
-            InitializeComponent();
+            this.InitializeComponent();
 
             TestHelper.AddGridCellBorders(this.LayoutRoot, 7, 3, Colors.AliceBlue);
+
+            RadialControllerConfiguration myConfiguration = RadialControllerConfiguration.GetForCurrentView();
+            myConfiguration.SetDefaultMenuItems(new RadialControllerSystemMenuItemKind[] { });
+
+            // Create a reference to the RadialController.
+            myController = RadialController.CreateForCurrentView();
+
+            // Disable standard menu??
+            myController.Menu.IsEnabled = false;
+
+            // Create an icon for the custom tool.
+            RandomAccessStreamReference icon =
+              RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Assets/dial_icon_custom_visual.png"));
+
+            // Create a menu item for the custom tool.
+            screenColorMenuItem =
+              RadialControllerMenuItem.CreateFromIcon("Screen Color", icon);
+
+            // Add the custom tool to the RadialController menu.
+            myController.Menu.Items.Add(screenColorMenuItem);
+
+            //screenColorMenuItem.Invoked += ColorMenuItem_Invoked;
+
+            myController.ScreenContactStarted += MyController_ScreenContactStarted;
+            myController.ScreenContactContinued += MyController_ScreenContactContinued;
+            myController.ScreenContactEnded += MyController_ScreenContactEnded;
+
+            myController.RotationChanged += MyController_RotationChanged;
         }
 
+        private async void MyController_ScreenContactStarted(RadialController sender, RadialControllerScreenContactStartedEventArgs args)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+            {
+                if (null == dialControl)
+                {
+                    dialControl = new Controls.DialColorControl();
+                    DialCanvas.Children.Add(dialControl);
+                }
+
+                dialControl.Width = 500;
+                dialControl.Height = 500;
+
+                Canvas.SetLeft(dialControl, args.Contact.Position.X - 250);
+                Canvas.SetTop(dialControl, args.Contact.Position.Y - 250);
+
+                dialControl.Visibility = Visibility.Visible;
+            });
+        }
+
+        private async void MyController_ScreenContactContinued(RadialController sender, RadialControllerScreenContactContinuedEventArgs args)
+        {
+            if (null != dialControl)
+            {
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    Canvas.SetLeft(dialControl, args.Contact.Position.X - 250);
+                    Canvas.SetTop(dialControl, args.Contact.Position.Y - 250);
+                });
+            }
+        }
+
+        private async void MyController_ScreenContactEnded(RadialController sender, object args)
+        {
+            if (null != dialControl)
+            {
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    dialControl.Visibility = Visibility.Collapsed;
+                });
+            }
+        }
+
+        private async void MyController_RotationChanged(RadialController sender, RadialControllerRotationChangedEventArgs args)
+        {
+            if (null != dialControl)
+            {
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    dialControl.Rotation += args.RotationDeltaInDegrees;
+                    if (dialControl.Rotation > 0)
+                    {
+                        dialControl.Rotation = 0;
+                    }
+                    else if (dialControl.Rotation < -315)
+                    {
+                        dialControl.Rotation = -315;
+                    }
+                    LayoutRoot.Background = dialControl.ColorBrush;
+                });
+            }
+        }
+
+        private void ColorMenuItem_Invoked(RadialControllerMenuItem sender, object args)
+        {
+            Debug.WriteLine("Item invoked");
+            myController.Menu.IsEnabled = false;
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            myController.Menu.Items.Remove(screenColorMenuItem);
+            Frame.GoBack();
+        }
     }
 }
