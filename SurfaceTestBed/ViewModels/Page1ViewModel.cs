982f8d035a35d4e552a75e8889fcb1ab7cb2be53
Patrick Strader <patrick_strader@hotmail.com>
2018-08-23 10:01:39 -0700

    Modified Page 1 to look like the actual apps with a hero and swipe left control.

--- SurfaceTestBed/ViewModels/Page1ViewModel.cs
+++ SurfaceTestBed/ViewModels/Page1ViewModel.cs
@@ -7,10 +7,13 @@ namespace SurfaceTestBed.ViewModels
     public class Page1ViewModel : ViewModelBase
     {
 
-        public string Headline = "Surface Test Bed - Page One";
-        public string Lede = "You can add your controls to this app to test them, and to test interaction.";
+        //public string Headline = "Surface Test Bed - Page One";
+        //public string Lede = "You can add your controls to this app to test them, and to test interaction.";
+
         public string HeroPhrase = "I Need A Hero!";
         public int HeroWordCount = 1;
+
+        public string SwipeLeft = "Swipe Left To Continue";
 
         public Page1ViewModel()
         {
