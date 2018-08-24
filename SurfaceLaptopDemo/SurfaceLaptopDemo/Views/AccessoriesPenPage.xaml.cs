9cb6fb9a6c61ca61ed38d6bb4551832612810cc2
Patrick Strader <patrick_strader@hotmail.com>
2018-08-24 11:26:28 -0700

    Created SurfaceLaptopDemo

---
+++ SurfaceLaptopDemo/SurfaceLaptopDemo/Views/AccessoriesPenPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using SurfaceLaptopDemo.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace SurfaceLaptopDemo.Views
+{
+    public sealed partial class AccessoriesPenPage : Page
+    {
+        private AccessoriesPenViewModel ViewModel
+        {
+            get { return DataContext as AccessoriesPenViewModel; }
+        }
+
+        public AccessoriesPenPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
