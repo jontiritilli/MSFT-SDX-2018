f2ac66b5360be9e83735b477bb8592ac4bdb8a48
Jonathan Tiritilli <jon.efmagency@outlook.com>
2018-08-28 11:12:54 -0700

    added battery animation, removed fadeinheader, modified header for batterypopup

--- SurfaceTestBed/SurfaceTestBed/Views/Page1Page.xaml.cs
+++ SurfaceTestBed/SurfaceTestBed/Views/Page1Page.xaml.cs
@@ -28,11 +28,6 @@ namespace SurfaceTestBed.Views
     /// </summary>
     public sealed partial class Page1Page : Page
     {
-
-        private RadialController _dialController;
-        private SurfaceDial _dial;
-        private RadialControllerMenuItem _screenColorMenuItem;
-
         private Page1ViewModel ViewModel
         {
             get { return DataContext as Page1ViewModel; }
@@ -42,88 +37,6 @@ namespace SurfaceTestBed.Views
         {
             InitializeComponent();
             //TestHelper.AddGridCellBorders(this.LayoutRoot, 7, 3, Colors.AliceBlue);
-        }
-
-        private void OnLoaded(object sender, RoutedEventArgs e)
-        {
-            // Create a reference to the RadialController.
-            _dialController = RadialController.CreateForCurrentView();
-
-            // Remove standard menu items
-            RadialControllerConfiguration _dialConfiguration = RadialControllerConfiguration.GetForCurrentView();
-            _dialConfiguration.SetDefaultMenuItems(new RadialControllerSystemMenuItemKind[] { });
-
-            // Create an icon for the custom tool.
-            RandomAccessStreamReference icon =
-                RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Assets/dial_icon_custom_visual.png"));
-
-            // Create a menu item for the custom tool.
-            _screenColorMenuItem =
-                RadialControllerMenuItem.CreateFromIcon("Screen Color", icon);
-
-            // Add the custom tool to the RadialController menu.
-            _dialController.Menu.Items.Add(_screenColorMenuItem);
-
-            //screenColorMenuItem.Invoked += ColorMenuItem_Invoked;
-
-            _dialController.ScreenContactStarted += OnControllerScreenContactStarted;
-
-            _dialController.ScreenContactContinued += OnControllerScreenContactContinued;
-
-            _dialController.ScreenContactEnded += OnControllerScreenContactEnded;
-
-            _dialController.RotationChanged += OnControllerRotationChanged;
-        }
-
-        private void OnControllerScreenContactStarted(RadialController sender, RadialControllerScreenContactStartedEventArgs args)
-        {
-            if (null == _dial)
-            {
-                _dial = new SurfaceDial();
-                DialCanvas.Children.Add(_dial);
-            }
-
-            _dial.Width = 500;
-            _dial.Height = 500;
-
-            Canvas.SetLeft(_dial, args.Contact.Position.X - 200);
-            Canvas.SetTop(_dial, args.Contact.Position.Y - 200);
-
-            _dial.Visibility = Visibility.Visible;
-        }
-
-        private void OnControllerScreenContactContinued(RadialController sender, RadialControllerScreenContactContinuedEventArgs args)
-        {
-            if (null != _dial)
-            {
-                Canvas.SetLeft(_dial, args.Contact.Position.X - 200);
-                Canvas.SetTop(_dial, args.Contact.Position.Y - 200);
-            }
-        }
-
-        private void OnControllerScreenContactEnded(RadialController sender, object args)
-        {
-            if (null != _dial)
-            {
-                _dial.Visibility = Visibility.Collapsed;
-            }
-        }
-
-        private void OnControllerRotationChanged(RadialController sender, RadialControllerRotationChangedEventArgs args)
-        {
-            if (null != _dial)
-            {
-                _dial.Rotation += args.RotationDeltaInDegrees;
-                if (_dial.Rotation > 0)
-                {
-                    _dial.Rotation = 0;
-                }
-                else if (_dial.Rotation < -315)
-                {
-                    _dial.Rotation = -315;
-                }
-                LayoutRoot.Background = _dial.ColorBrush;
-            }
         }
     }
 }
