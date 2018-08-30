1b25b15da6bbb3536a7af2fff4ec0b951058f209
Jonathan Tiritilli <jon.efmagency@outlook.com>
2018-08-30 11:13:40 -0700

    added test code to page 3

--- SurfaceTestBed/SurfaceTestBed/Views/Page3Page.xaml.cs
+++ SurfaceTestBed/SurfaceTestBed/Views/Page3Page.xaml.cs
@@ -4,6 +4,7 @@ using Windows.UI;
 using Windows.UI.Xaml.Controls;
 
 using SurfaceTestBed.ViewModels;
+using SDX.Toolkit.Controls;
 using SDX.Toolkit.Helpers;
 
 
@@ -16,11 +17,21 @@ namespace SurfaceTestBed.Views
             get { return DataContext as Page3ViewModel; }
         }
 
+        ListItem[] items = new ListItem[4];
+
         public Page3Page()
         {
             InitializeComponent();
 
             TestHelper.AddGridCellBorders(this.LayoutRoot, 7, 3, Colors.Orange);
+
+            items[0] = ListItem.CreateListItem(0, ListItemIcon.Jot, 50d, null, ViewModel.BulletJot, null);
+
+            items[1] = ListItem.CreateListItem(1, ListItemIcon.Write, 50d, ViewModel.HeadlineWrite, ViewModel.BulletWrite, null);
+
+            items[2] = ListItem.CreateListItem(2, ListItemIcon.Pressure, 50d, ViewModel.HeadlinePressure, ViewModel.BulletPressure, null);
+
+            items[3] = ListItem.CreateListItem(3, ListItemIcon.Palm, 50d, ViewModel.HeadlinePalm, ViewModel.BulletPalm, null);
         }
     }
 }
