0f40a5ef975e704b66c9590c472c0be81983a7b2
Patrick Strader <patrick_strader@hotmail.com>
2018-09-17 13:38:49 -0700

    Added MultiplyDoublesConverter to SDX.Toolkit.Converters

---
+++ SDX.Toolkit/SDX.Toolkit/Converters/MultiplyDoublesConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+using SDX.Toolkit.Helpers;
+
+
+namespace SDX.Toolkit.Converters
+{
+    public class MultiplyDoublesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, String language)
+        {
+            double operand1 = 0;
+            double resultDouble = 0;
+
+            if (null != value)
+            {
+                if (value is Double valueDouble)
+                {
+                    operand1 = valueDouble;
+                }
+            }
+            if (null != parameter)
+            {
+                if (parameter is string paramString)
+                {
+                    double operand2;
+
+                    if (Double.TryParse(paramString, out operand2))
+                    {
+                        resultDouble = operand1 * operand2;
+                    }
+                }
+            }
+
+            return resultDouble;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, String language)
+        {
+            throw new NotSupportedException("Only one way bindings are supported with this converter");
+        }
+    }
+}
+
