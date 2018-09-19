83b92a2cc1586bcaa295f01fb06e3b93522940b9
Jay Novilla <jay.efmagency@outlook.com>
2018-09-19 13:59:13 -0700

    Compare page in all apps

--- SurfaceBook2Demo/SurfaceBook2Demo/ViewModels/CompareViewModel.cs
+++ SurfaceBook2Demo/SurfaceBook2Demo/ViewModels/CompareViewModel.cs
@@ -10,19 +10,75 @@ namespace SurfaceBook2Demo.ViewModels
 {
     public class CompareViewModel : ViewModelBase
     {
+
+
         #region Constants
 
-        private const string URI_BACKGROUND = "ms:appx///Assets/Backgrounds/gradient-bg.jpg";
+        private const string URI_BACKGROUND = "ms-appx:///Assets/Backgrounds/sb2_15_background_light.jpg";
 
+        private const string URI_FAMILY = "ms-appx:///Assets/Comparison/comparisonFamily.png";
+        private const int IMG_FAMILY_WIDTH = 1368;//2736
+        private const int IMG_FAMILY_HEIGHT = 492;//984
+
+        private const string URI_PRO = "ms-appx:///Assets/Comparison/comparisonCruz.png";
+        private const int IMG_PRO_WIDTH = 702;//1404
+        private const int IMG_PRO_HEIGHT = 630;//1260
+
+        private const string URI_BOOK = "ms-appx:///Assets/Comparison/comparisonSb2.png";
+        private const int IMG_BOOK_WIDTH = 702;//1404
+        private const int IMG_BOOK_HEIGHT = 630;//1260
+
+        private const string URI_STUDIO = "ms-appx:///Assets/Comparison/comparisonCaprock.png";
+        private const int IMG_STUDIO_WIDTH = 702;//1404
+        private const int IMG_STUDIO_HEIGHT = 630;//1260
+
+        private const string URI_LAPTOP = "ms-appx:///Assets/Comparison/comparisonFoxburg.png";
+        private const int IMG_LAPTOP_WIDTH = 702;//1404
+        private const int IMG_LAPTOP_HEIGHT = 630;//1260
+
+        private const string URI_GO = "ms-appx:///Assets/Comparison/comparisonGo.png";
+        private const int IMG_GO_WIDTH = 702;//1404
+        private const int IMG_GO_HEIGHT = 630;//1260
         #endregion
 
 
         #region Public Properties
 
+        public string Headline;
+        public string Lede;
+
         public string BackgroundUri = URI_BACKGROUND;
 
-        #endregion
+        public string FamilyUri = URI_FAMILY;
+        public int FamilyHeight = IMG_FAMILY_HEIGHT;
+        public int FamilyWidth = IMG_FAMILY_WIDTH;
 
+        public string ProUri = URI_PRO;
+        public int ProHeight = IMG_PRO_HEIGHT;
+        public int ProWidth = IMG_PRO_WIDTH;
+
+        public string BookUri = URI_BOOK;
+        public int BookHeight = IMG_BOOK_HEIGHT;
+        public int BookWidth = IMG_BOOK_WIDTH;
+
+        public string StudioUri = URI_STUDIO;
+        public int StudioHeight = IMG_STUDIO_HEIGHT;
+        public int StudioWidth = IMG_STUDIO_WIDTH;
+
+        public string LaptopUri = URI_LAPTOP;
+        public int LaptopHeight = IMG_LAPTOP_HEIGHT;
+        public int LaptopWidth = IMG_LAPTOP_WIDTH;
+
+        public string GoUri = URI_GO;
+        public int GoHeight = IMG_GO_HEIGHT;
+        public int GoWidth = IMG_GO_WIDTH;
+
+        public string ProTitle;
+        public string BookTitle;
+        public string StudioTitle;
+        public string LaptopTitle;
+        public string GoTitle;
+        #endregion
 
         #region Construction
 
--- SurfaceProDemo/SurfaceProDemo/Views/ComparePage.xaml.cs
+++ SurfaceProDemo/SurfaceProDemo/Views/ComparePage.xaml.cs
@@ -24,6 +24,12 @@ namespace SurfaceProDemo.Views
         public ComparePage()
         {
             InitializeComponent();
+            this.rBtnPro.PopupChild = this.PopPro;
+            this.rBtnBook.PopupChild = this.PopBook;
+            this.rBtnStudio.PopupChild = this.PopStudio;
+            this.rBtnLaptop.PopupChild = this.PopLaptop;
+            this.rBtnGo.PopupChild = this.PopGo;
+
         }
 
         #endregion
@@ -34,11 +40,45 @@ namespace SurfaceProDemo.Views
         public void NavigateToPage()
         {
             // animations in
+            SDX.Toolkit.Helpers.AnimationHelper.PerformPageEntranceAnimation(this);
+            SDX.Toolkit.Helpers.AnimationHelper.PerformTranslateIn(this.img_Family, this.img_Family.TranslateDirection, 100, 500, 0);
+
+            rBtnPro.StartEntranceAnimation();
+            rBtnPro.StartRadiateAnimation();
+
+            rBtnBook.StartEntranceAnimation();
+            rBtnBook.StartRadiateAnimation();
+
+            rBtnStudio.StartEntranceAnimation();
+            rBtnStudio.StartRadiateAnimation();
+
+            rBtnLaptop.StartEntranceAnimation();
+            rBtnLaptop.StartRadiateAnimation();
+
+            rBtnGo.StartEntranceAnimation();
+            rBtnGo.StartRadiateAnimation();
+
+
         }
 
         public void NavigateFromPage()
         {
             // animations out
+            SDX.Toolkit.Helpers.AnimationHelper.PerformPageExitAnimation(this);
+            rBtnPro.ResetEntranceAnimation();
+            rBtnPro.ResetRadiateAnimation();
+
+            rBtnBook.ResetEntranceAnimation();
+            rBtnBook.ResetRadiateAnimation();
+
+            rBtnStudio.ResetEntranceAnimation();
+            rBtnStudio.ResetRadiateAnimation();
+
+            rBtnLaptop.ResetEntranceAnimation();
+            rBtnLaptop.ResetRadiateAnimation();
+
+            rBtnGo.ResetEntranceAnimation();
+            rBtnGo.ResetRadiateAnimation();
         }
 
         #endregion
