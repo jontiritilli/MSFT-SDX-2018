7e268d2c8412c9d322fc78df42eb3a4dbead6ca1
Patrick Strader <patrick_strader@hotmail.com>
2018-10-08 13:52:47 -0700

    Safety Check-In

--- YogaC930AudioDemo/YogaC930AudioDemo/App.xaml.cs
+++ YogaC930AudioDemo/YogaC930AudioDemo/App.xaml.cs
@@ -1,10 +1,15 @@
 ﻿using System;
-
+using GalaSoft.MvvmLight.Ioc;
+using SDX.Telemetry.Services;
+using SDX.Toolkit.Helpers;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
+using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 
 using YogaC930AudioDemo.Services;
+
 
 namespace YogaC930AudioDemo
 {
@@ -23,8 +28,52 @@ namespace YogaC930AudioDemo
 
             EnteredBackground += App_EnteredBackground;
 
+            // we want full screen, but leave this off during dev 
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.FullScreen;
+            //ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.Auto;
+
+            // we want landscape only
+            DisplayOrientations orientations = DisplayOrientations.Landscape;
+            DisplayInformation.AutoRotationPreferences = orientations;
+
             // Deferred execution until used. Check https://msdn.microsoft.com/library/dd642331(v=vs.110).aspx for further info on Lazy<T> class.
             _activationService = new Lazy<ActivationService>(CreateActivationService);
+
+            // register our configuration service and initialize it
+            SimpleIoc.Default.Register<ConfigurationService>();
+            ConfigurationService configurationService = (ConfigurationService)SimpleIoc.Default.GetInstance<ConfigurationService>();
+            if (null != configurationService)
+            {
+                // run this synchronously 
+                AsyncHelper.RunSync(() => configurationService.Initialize());
+            }
+
+            // register our localization service and initialize it
+            SimpleIoc.Default.Register<LocalizationService>();
+            LocalizationService localizationService = (LocalizationService)SimpleIoc.Default.GetInstance<LocalizationService>();
+            if (null != localizationService)
+            {
+                // async here might lead to a race condition, but no signs so far
+                //localizationService.Initialize();
+                AsyncHelper.RunSync(() => localizationService.Initialize());
+            }
+
+            // initialize the telemetry service
+            SimpleIoc.Default.Register<TelemetryService>();
+            TelemetryService telemetryService = (TelemetryService)SimpleIoc.Default.GetInstance<TelemetryService>();
+            if (null != telemetryService)
+            {
+                if (null != configurationService)
+                {
+                    // DO NOT try to run this asynchronously; MetroLog hangs when invoked async
+                    //AsyncHelper.RunSync(() => telemetryService.Initialize(configurationService.Configuration.TelemetryKey));
+                    telemetryService.Initialize(configurationService.Configuration.IsTelemetryEnabled,
+                                                configurationService.Configuration.TelemetryKey);
+
+                    // log app start
+                    TelemetryService.Current?.LogTelemetryEvent(TelemetryEvents.StartApplication);
+                }
+            }
         }
 
         protected override async void OnLaunched(LaunchActivatedEventArgs args)
---
+++ YogaC930AudioDemo/YogaC930AudioDemo/Models/ConfigurationFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SDX.Telemetry.Services;
+
+
+namespace YogaC930AudioDemo.Models
+{
+    public class ConfigurationFile
+    {
+
+        public bool IsAttractorLoopEnabled = true;
+        public string AttractorLoopFileName = "ms-appx:///Assets/Attractor/Lenovo_Yoga_C930_Attract_Loop_240718.mp4";
+        public string Language = "en-US";
+        public bool IsTelemetryEnabled = false;
+        public TelemetryKeys TelemetryKey = TelemetryKeys.Test;
+
+        public static ConfigurationFile CreateDefault()
+        {
+            ConfigurationFile config = new ConfigurationFile()
+            {
+                IsAttractorLoopEnabled = true,
+                Language = String.Empty,
+                IsTelemetryEnabled = false,
+                TelemetryKey = TelemetryKeys.Test,
+            };
+
+            return config;
+        }
+    }
+}
