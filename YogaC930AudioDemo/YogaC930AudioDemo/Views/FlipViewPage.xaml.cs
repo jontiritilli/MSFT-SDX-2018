52eb17c58287c80b17e5ce87e8ae29abf5da89d2
Patrick Strader <patrick_strader@hotmail.com>
2018-10-17 13:31:59 -0700

    Re-doing work I lost
    Former-commit-id: 814d87a2c5513070630e517b89dafc8f39d985c7

--- YogaC930AudioDemo/YogaC930AudioDemo/Views/FlipViewPage.xaml.cs
+++ YogaC930AudioDemo/YogaC930AudioDemo/Views/FlipViewPage.xaml.cs
@@ -9,24 +9,64 @@ namespace YogaC930AudioDemo.Views
 {
     public sealed partial class FlipViewPage : Page
     {
+        #region Public Static Methods
+
+        public static FlipViewPage Current { get; private set; }
+
+        #endregion
+
+
+        #region Public Properties
+
         private FlipViewViewModel ViewModel
         {
             get { return DataContext as FlipViewViewModel; }
         }
-        #region public static
-        public static FlipViewPage Current { get; private set; }
+
         #endregion
+
+
+        #region Construction
+
         public FlipViewPage()
         {
             InitializeComponent();
+
             FlipViewPage.Current = this;
+
             this.Loaded += FlipViewPage_Loaded;
         }
+
+        #endregion
+
+
+        #region Event Handlers
 
         private void FlipViewPage_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
             PlayerPopupPage.Current.CloseButton_Clicked += Close_Player_Clicked;
         }
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            if (this.PlayerPopup.IsOpen == false)
+            {
+                this.PlayerPopup.IsOpen = true;
+            }
+        }
+
+        private void Close_Player_Clicked(object sender, RoutedEventArgs e)
+        {
+            if (this.PlayerPopup.IsOpen == true)
+            {
+                this.PlayerPopup.IsOpen = false;
+            }
+        }
+
+        #endregion
+
+
+        #region Public Methods
 
         public Popup GetPlayerPopup()
         {
@@ -45,20 +85,6 @@ namespace YogaC930AudioDemo.Views
 
         }
 
-        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
-        {
-            if (this.PlayerPopup.IsOpen == false)
-            {
-                this.PlayerPopup.IsOpen = true;
-            }
-        }
-
-        private void Close_Player_Clicked(object sender, RoutedEventArgs e)
-        {
-            if (this.PlayerPopup.IsOpen == true)
-            {
-                this.PlayerPopup.IsOpen = false;
-            }
-        }
+        #endregion
     }
 }
