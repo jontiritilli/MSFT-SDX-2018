a96055b5edcb3558038d72835260daef232a90aa
Patrick Strader <patrick_strader@hotmail.com>
2018-10-19 14:35:43 -0700

    H.265 and FlipView Updates
    Former-commit-id: 88e2800f20faa41f7b522638e666ac7b5f1ac000

---
+++ YogaC930AudioDemo/YogaC930AudioDemo/Helpers/StyleHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.UI.Xaml;
+
+
+namespace YogaC930AudioDemo.Helpers
+{
+    public static class StyleHelper
+    {
+        public static Style GetApplicationStyle(String name)
+        {
+            Style style = null;
+            try
+            {
+                style = (Style)Application.Current.Resources[name];
+            }
+            catch
+            {
+            }
+
+            return style;
+        }
+    }
+}
