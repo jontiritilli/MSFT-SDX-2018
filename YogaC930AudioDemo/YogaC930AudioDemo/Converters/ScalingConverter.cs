1599cc2e8d420dfe763556762e585fe1184970c2
Patrick Strader <patrick_strader@hotmail.com>
2018-10-22 16:06:59 -0700

    Added new converter to handle resolution/scale.
    Former-commit-id: 37c427eecd3d782a4330a56ec8ec25ea34d48a0a

---
+++ YogaC930AudioDemo/YogaC930AudioDemo/Converters/ScalingConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.UI.Xaml.Data;
+
+using YogaC930AudioDemo.Helpers;
+
+
+namespace YogaC930AudioDemo.Converters
+{
+
+    public class ScalingConverter : IValueConverter
+    {
+        private const double DEFAULT_RESOLUTION = 1920;
+
+        private double _resolutionX;
+        private double _scale;
+
+        public ScalingConverter()
+        {
+            Size resolution = WindowHelper.GetScreenResolutionInfo();
+            _resolutionX = resolution.Width;
+            if (resolution.Height > _resolutionX) { _resolutionX = resolution.Height; }
+
+            _scale = WindowHelper.GetRawPixelsPerViewPixel();
+        }
+
+        public object Convert(object value, Type targetType, object parameter, String language)
+        {
+            double result = 0;
+
+            if ((null != value) && (value is double doubleValue))
+            {
+                result = doubleValue * (_resolutionX / (DEFAULT_RESOLUTION * _scale));
+            }
+
+            return result;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, String language)
+        {
+            throw new NotSupportedException("Only one way bindings are supported with this converter");
+        }
+    }
+}
