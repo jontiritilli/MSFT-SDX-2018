509fd46c15a57980831bb5240a0bd7e20634f9fd
Jonathan Tiritilli <jon.efmagency@outlook.com>
2018-10-23 17:06:54 -0700

    fix activation service for studio
    Former-commit-id: 4992b345cd8c0a9972a7878c122075fef0a5685b

---
+++ SurfaceStudioDemo/SurfaceStudioDemo/Activation/ActivationHelper.cs
@@ -0,0 +1,52 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using GalaSoft.MvvmLight.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+
+using SurfaceStudioDemo.Services;
+using SDX.Toolkit.Helpers;
+using Windows.UI.Xaml;
+
+namespace SurfaceStudioDemo.Activation
+{
+    class ActivationHelper
+    {
+        #region Private Static Members
+
+        private static NavigationServiceEx NavigationService
+        {
+            get
+            {
+                return CommonServiceLocator.ServiceLocator.Current.GetInstance<NavigationServiceEx>();
+            }
+        }
+
+        #endregion
+
+        #region Public Static Methods
+
+        public static void LoadAppResourceDictionaries()
+        {
+            // get the localization service
+            LocalizationService localizationService = SimpleIoc.Default.GetInstance<LocalizationService>();
+
+            string file = localizationService.IsLanguageJapanese() ? "TextBlock-ja-JP.xaml" : "TextBlock.xaml";
+
+            // calculate uri's for styles 
+            string URI_TEXTBLOCK = String.Format("ms-appx:///Styles/{0}", file);
+
+            // load textblock styles
+            ResourceDictionary resourceDictionary = new ResourceDictionary()
+            {
+                Source = new Uri(URI_TEXTBLOCK, UriKind.Absolute),
+            };
+            Application.Current.Resources.MergedDictionaries.Add(resourceDictionary);
+        }
+
+        #endregion
+    }
+}
